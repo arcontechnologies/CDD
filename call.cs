da59dcb53a643471f801b4565d820800535bb6f6
Arcon Technologies <arcondo.dasilva@gmail.com>
2026-01-27 19:45:43 +0100

    Implement parallel loading for tasks table

---
+++ call.cs
@@ -0,0 +1,4 @@
+DataSet SQLTasks = new DataSet(); // This is just a placeholder now
+logger.Info("Load for tasks table has started (parallel)");
+LoadSubDatasetParallel(SQLTasks, ConfigTable, "tasks", 2);
+logger.Info("Load for tasks table has completed");
