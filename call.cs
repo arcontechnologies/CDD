8f9de78ef4d8556d476e1a7c7a8fa16802ec431c
Arcon Technologies <arcondo.dasilva@gmail.com>
2026-01-28 20:14:22 +0100

    Update call.cs

--- call.cs
+++ call.cs
@@ -2,3 +2,7 @@ DataSet SQLTasks = new DataSet(); // This is just a placeholder now
 logger.Info("Load for tasks table has started (parallel)");
 LoadSubDatasetParallel(SQLTasks, ConfigTable, "tasks", 2);
 logger.Info("Load for tasks table has completed");
+
+
+
+MainModule = 'proc.MainModule' threw an exception of type 'System.ComponentModel.Win32Exception'
