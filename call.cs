c7833a1e926558ec6826adb3baba580345859592
Arcon Technologies <arcondo.dasilva@gmail.com>
2026-01-28 20:37:19 +0100

    Update call.cs

--- call.cs
+++ call.cs
@@ -6,3 +6,14 @@ logger.Info("Load for tasks table has completed");
 
 
 MainModule = 'proc.MainModule' threw an exception of type 'System.ComponentModel.Win32Exception'
+
+string output = proc.StandardOutput.ReadToEnd();
+string error = proc.StandardError.ReadToEnd();
+  
+task load for release table has started
+task load for application table has started
+task load for environment table has started
+task load for tracks table has started
+
+CustomAttributes = Method System.Reflection.MemberInfo.get_CustomAttributes cannot be called in this context.
+StandardInput = 'proc.StandardInput' threw an exception of type 'System.InvalidOperationException'
